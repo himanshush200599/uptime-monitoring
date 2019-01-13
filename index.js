9e174ffd3429460e7899978ffb407013b4b4c5c5
himanshush200599 <himanshush214@gmail.com>
2019-01-13 09:33:13 -0500

    start with index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// This is primary file for  API
+// Here we are building RESTful API in pure nodejs without any  framework
+// and library.
+// This will indicate whether a site is UP or DOWN by sending SMS on mobile
+// phone
