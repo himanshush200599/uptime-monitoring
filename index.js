a480bee12c5891e4906034e5064e731239f4cf12
himanshush200599 <himanshush214@gmail.com>
2019-01-13 10:07:30 -0500

    setting up server and parsed URL

--- index.js
+++ index.js
@@ -3,3 +3,19 @@
 // and library.
 // This will indicate whether a site is UP or DOWN by sending SMS on mobile
 // phone
+var http = require("http");
+var url = require("url");
+var server = http.createServer(function(req, res) {
+  var parsedUrl = url.parse(req.url, true);
+
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  res.end("Hello world\n");
+
+  console.log("Request received on path: " + trimmedPath);
+});
+
+server.listen(3000, function() {
+  console.log("The server is listening on port 3000");
+});
