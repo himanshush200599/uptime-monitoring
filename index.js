6011c7c2b1e04ba6bb43189f5bf71662eb492648
himanshush200599 <himanshush214@gmail.com>
2019-01-13 10:52:55 -0500

    Handles payload(Body sent in POST request)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 // phone
 var http = require("http");
 var url = require("url");
+var StringDecoder = require("string_decoder").StringDecoder;
+
 var server = http.createServer(function(req, res) {
   var parsedUrl = url.parse(req.url, true);
 
@@ -13,17 +15,28 @@ var server = http.createServer(function(req, res) {
   var queryStringObject = parsedUrl.query;
   var method = req.method.toLowerCase();
   var headers = req.headers;
-  res.end("Hello world\n");
 
-  console.log(
-    "Request received on path: " +
-      trimmedPath +
-      " with method " +
-      method +
-      " with query parameters :" +
-      queryStringObject
-  );
-  console.log("Headers are :" + headers);
+  var decoder = new StringDecoder("utf-8");
+  var buffer = "";
+  req.on("data", function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function() {
+    buffer += decoder.end();
+
+    res.end("Hello world\n");
+
+    console.log(
+      "Request received on path: " +
+        trimmedPath +
+        " with method " +
+        method +
+        " with query parameters :" +
+        queryStringObject
+    );
+    console.log("Headers are :" + headers);
+    console.log("Payload is " + buffer);
+  });
 });
 
 server.listen(3000, function() {
