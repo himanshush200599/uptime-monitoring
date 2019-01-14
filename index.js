258dec46c9c86f79e329fb9801a446afa43fad44
himanshush200599 <himanshush214@gmail.com>
2019-01-14 05:38:14 -0500

    write comments on whole code

--- index.js
+++ index.js
@@ -3,20 +3,35 @@
 // and library.
 // This will indicate whether a site is UP or DOWN by sending SMS on mobile
 // phone
+
+//reuired dependencies
 var http = require("http");
 var url = require("url");
+
+//string decoder modules
 var StringDecoder = require("string_decoder").StringDecoder;
 
+//create server and running
 var server = http.createServer(function(req, res) {
+  //parsed url var
   var parsedUrl = url.parse(req.url, true);
-
+  //extracr relative url path
   var path = parsedUrl.pathname;
+  //trim url (slases)
   var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  //extract query  parameters
   var queryStringObject = parsedUrl.query;
+
+  //find methods of request
   var method = req.method.toLowerCase();
+
+  //headers in post request
   var headers = req.headers;
 
   var decoder = new StringDecoder("utf-8");
+
+  //read payload header in buffer
   var buffer = "";
   req.on("data", function(data) {
     buffer += decoder.write(data);
